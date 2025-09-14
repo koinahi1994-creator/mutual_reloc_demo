99138a4cf34b47f7dff0c608a4fe06c3e1d6e0dd
koinahi1994-creator <koinahi1994@gmail.com>
2025-09-14 11:47:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+import { app, analytics } from './firebaseConfig.js';
+
+// Placeholder logic for signup, PF input, branch preference, and matching
+console.log("Mutual Relocation Demo App initialized");
+console.log("PF Number and branch preference will be stored here");
